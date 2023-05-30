7dcff368945d0783fbeb387a0b9457ea291b84a2
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-05-30 17:26:48 +0200

    added funny bg

--- js/script.js
+++ js/script.js
@@ -6,16 +6,15 @@ for (let i = 1; i <= 100; i++){
     number = i;
     if(number % 5 !== 0 && number % 3 == 0){
         console.log(number = "Fizz");
-        target.innerHTML += `<p class="paragraph bg-success">${number}</p>`;
+        target.innerHTML += `<p class="paragraph bg-marius text-danger">${number}</p>`;
     } else if(number % 5 == 0 && number % 3 !== 0){
         console.log(number = "Buzz");
-        target.innerHTML += `<p class="paragraph bg-warning">${number}</p>`;
+        target.innerHTML += `<p class="paragraph bg-marcoB text-info">${number}</p>`;
     } else if(number % 5 == 0 && number % 3 == 0){
         console.log(number = "FizzBuzz");
-        target.innerHTML += `<p class="paragraph bg-danger">${number}</p>`;
+        target.innerHTML += `<p class="paragraph bg-marcoL text-warning">${number}</p>`;
     } else {
         console.log(number);
         target.innerHTML += `<p class="paragraph bg-primary">${number}</p>`;
     }
-    
 }
