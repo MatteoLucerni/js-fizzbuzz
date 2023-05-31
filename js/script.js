688fd90b46136d75652743b5f6e19efde5e6fef3
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-05-31 11:46:03 +0200

    optimized code

--- js/script.js
+++ js/script.js
@@ -2,19 +2,23 @@ console.log('JS OK');
 
 const target = document.getElementById('target');
 
+let paragraph = '';
+
 for (let i = 1; i <= 100; i++){
     number = i;
     if(number % 5 !== 0 && number % 3 == 0){
         console.log(number = "Fizz");
-        target.innerHTML += `<p class="paragraph bg-marius text-danger">${number}</p>`;
+        paragraph += `<p class="paragraph bg-marius text-danger">${number}</p>`;
     } else if(number % 5 == 0 && number % 3 !== 0){
         console.log(number = "Buzz");
-        target.innerHTML += `<p class="paragraph bg-marcoB text-info">${number}</p>`;
+        paragraph += `<p class="paragraph bg-marcoB text-info">${number}</p>`;
     } else if(number % 5 == 0 && number % 3 == 0){
         console.log(number = "FizzBuzz");
-        target.innerHTML += `<p class="paragraph bg-marcoL text-warning">${number}</p>`;
+        paragraph += `<p class="paragraph bg-marcoL text-warning">${number}</p>`;
     } else {
         console.log(number);
-        target.innerHTML += `<p class="paragraph bg-primary">${number}</p>`;
+        paragraph += `<p class="paragraph bg-primary">${number}</p>`;
     }
 }
+
+target.innerHTML = paragraph;
